c27b38f47375d96404614031cdb796852666d66f
Kássio San <kciosantana@gmail.com>
2023-08-15 16:28:57 -0300

    Modes webpack

---
+++ modes/src/components/heading.js
@@ -0,0 +1,14 @@
+
+export default class Heading {
+    create(title) {
+
+        const h1 = document.createElement('h1');
+
+        h1.innerText = title;
+
+        h1.classList.add('title')
+
+        document.querySelector('body').appendChild(h1);
+
+    }
+}
---
+++ modes/src/index.js
@@ -0,0 +1,12 @@
+import Heading from "./components/heading";
+
+const heading = new Heading();
+
+
+heading.create("Main page")
+
+console.log(VERSION);
+console.log(PORT);
+
+// accessing .env file
+console.log(process.env.API_KEY);
