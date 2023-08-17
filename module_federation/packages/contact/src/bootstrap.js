f19921fffa0a17871f76c83fc35f0d72343ff6de
Kássio San <kciosantana@gmail.com>
2023-08-17 20:06:59 -0300

    third application Contacts React

---
+++ module_federation/packages/contact/src/bootstrap.js
@@ -0,0 +1,8 @@
+import Contact from './Contact';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+// bootstrap means initialize : "Inicializar"
+ReactDOM.render(<Contact/>, document.querySelector('#root'));
