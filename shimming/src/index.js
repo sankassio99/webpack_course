ae4dd9734e9ac26e0770883b6017ec46c7b8c831
Kássio San <kciosantana@gmail.com>
2023-08-22 21:38:15 -0300

    Shimming

---
+++ shimming/src/index.js
@@ -0,0 +1,7 @@
+console.log(_.difference([1,2,3], [1,4,5]));
+
+
+console.log($)
+
+
+console.log(Vue);
---
+++ shimming/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path")
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    plugins: [
+        new webpack.ProvidePlugin({
+            _: 'lodash',
+            $: 'jquery',
+            Vue: 'vue'
+        }),
+    ]
+}
